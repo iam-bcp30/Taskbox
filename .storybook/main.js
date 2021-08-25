39d28c307088211a7ed3d0e03d555c99ae94b3e6
iam-bcp30 <52638902+iam-bcp30@users.noreply.github.com>
2021-08-25 11:14:17 -0500

    Created 1st component

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,9 @@
 module.exports = {
   "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    '../src/components/**/*.stories.js'
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
-  ]
-}
+  ],
+};
